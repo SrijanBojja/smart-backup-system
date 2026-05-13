e1792958e0d9266921eaa3c241bed2cb700269c1
SrijanBojja <srijan.bojja@gmail.com>
2026-05-13 13:17:43 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ require('./config/db');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
