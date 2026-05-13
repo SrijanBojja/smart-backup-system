093761b6a21b98c2f6ec29c014d708be7572d8ca
SrijanBojja <srijan.bojja@gmail.com>
2026-05-13 14:22:43 +0530

    final fix mysql_url connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err.message);
+    console.error("Database connection failed:", err);
   } else {
     console.log("MySQL Connected Successfully");
   }
