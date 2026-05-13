6450c844978bea04db4699f5b034c550c5ca67b8
SrijanBojja <srijan.bojja@gmail.com>
2026-05-13 13:12:24 +0530

    fixed API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://smart-backup-system-cxqg.onrender.com'
 });
 
 export default API;
